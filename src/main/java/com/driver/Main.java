477f722ac5bb3abd8614a57b78f97f57592f5223
Deepesh Gupta <guptad28031998@gmail.com>
2023-08-10 11:04:09 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,26 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+
+        public int product(int x , int y){
+            return 1;
+        }
+
+        public int product(int x , int y , int z){
+            return 2;
+        }
+
+        public double product(double x , double y){
+            return 3.0;
+        }
+    }
+
+    public static void main(String agrs[]){
+        Product p = new Product();
+        System.out.println(p.product(1,0));
+        System.out.println(p.product(1,0,2));
+        System.out.println(p.product(1.0 , 2.0));
+    }
+
 }
